da2e81cfad0f9849fed91041146c488aae5b2b2a
Bruno Becoski <brunobecoski@gmail.com>
2025-11-22 17:05:30 -0300

    Aula 3

--- scripts.js
+++ scripts.js
@@ -1,9 +1,38 @@
-let data = []
+const cardContainer__element = document.getElementById('card-container')
+const input__element = document.getElementById('search-input')
+
+let apiData = []
 
 async function handleSearch() {
-  const response = await fetch('data.json')
+  if  (apiData.length === 0) {
+    try {
+      const response = await fetch('data.json')
+      apiData = await response.json()
+    } catch (error) {
+      console.error('Falha ao buscar dados: ', error)
+      return
+    } 
+  }
 
-  data = await response.json()
+  const searchTerm = input__element.value.toLowerCase()
+  const filteredData = apiData.filter(data =>
+    data.name.toLowerCase().includes(searchTerm) || data.description.toLowerCase().includes(searchTerm)
+  )
 
-  console.log(data)
+  renderCards(filteredData)
+}
+
+function renderCards(filteredData) {
+  for (let data of filteredData) {
+    const article__element = document.createElement('article')
+
+    article__element.innerHTML = `
+      <h2>${data.name}</h2>
+      <p>${data.year}</p>
+      <p>${data.description}</p>
+      <a href="${data.link}" target="_blank">Saiba mais</a>
+    `
+
+    cardContainer__element.appendChild(article__element)
+  }
 }
