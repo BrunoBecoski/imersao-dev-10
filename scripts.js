9f1d92b4653cd19806a59219e9698ea7dfd6487a
Bruno Becoski <brunobecoski@gmail.com>
2025-12-02 20:18:22 -0300

    Atualiza o scripts.js

--- scripts.js
+++ scripts.js
@@ -5,6 +5,48 @@ const section__element = document.getElementById('section')
 
 let apiData = []
 
+checkbox__element.addEventListener('change', (event) => {
+  const isChecked = event.target.checked
+  const optionSelected = select__element.value
+
+  if(isChecked) {
+   const filteredData = filterData()
+
+    if(optionSelected === 'alphabet') {
+      const orderFilteredData = orderByAlphabet(filteredData)
+
+      renderCards(orderFilteredData)
+    }
+
+    if (optionSelected === 'create') {
+      const orderFilteredData = orderByCreation(filteredData)
+
+      renderCards(orderFilteredData)
+    }
+  }
+})
+
+select__element.addEventListener('change', (event) => {
+  const optionSelected = event.target.value 
+  const isChecked = checkbox__element.checked
+
+  if(isChecked) {
+   const filteredData = filterData()
+
+    if(optionSelected === 'alphabet') {
+      const orderFilteredData = orderByAlphabet(filteredData)
+
+      renderCards(orderFilteredData)
+    }
+
+    if (optionSelected === 'create') {
+      const orderFilteredData = orderByCreation(filteredData)
+
+      renderCards(orderFilteredData)
+    }
+  }
+})
+
 async function handleSearch() {
   if  (apiData.length === 0) {
     try {
@@ -13,21 +55,53 @@ async function handleSearch() {
     } catch (error) {
       console.error('Falha ao buscar dados: ', error)
       return
-    } 
+    }
   }
 
-  const searchTerm = input__element.value.toLowerCase()
-  const filteredData = filterData(searchTerm)
+  checkbox__element.checked = false
+
+  const filteredData = filterData()
 
   renderCards(filteredData)
 }
 
-function filterData(searchTerm) {
+function filterData() {
+  const searchTerm = input__element.value.toLowerCase()
   const filteredData = apiData.filter(data =>
     data.name.toLowerCase().includes(searchTerm) || data.description.toLowerCase().includes(searchTerm)
   )
 
   return filteredData
+}
+
+function orderByAlphabet(filteredData) {
+  const orderFilteredData = filteredData.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1
+    }
+
+    if (a.name > b.name) {
+      return 1
+    }
+    return 0
+  })
+
+  return orderFilteredData
+}
+
+function orderByCreation(filteredData) {
+  const orderFilteredData = filteredData.sort((a, b) => {
+    if (a.year < b.year) {
+      return -1
+    }
+
+    if (a.year > b.year) {
+      return 1
+    }
+    return 0
+  })
+
+  return orderFilteredData
 }
 
 function renderCards(filteredData) {
@@ -49,23 +123,3 @@ function renderCards(filteredData) {
     section__element.appendChild(article__element)
   }
 }
-
-checkbox__element.addEventListener('change', (event) => {
-  const isChecked = event.target.checked
-  const optionSelected = select__element.value
-  const searchTerm = input__element.value.toLowerCase()
-
-  console.log(isChecked)
-  console.log(optionSelected)
-  console.log(searchTerm)
-})
-
-select__element.addEventListener('change', (event) => {
-  const optionSelected = event.target.value 
-  const isChecked = checkbox__element.checked
-  const searchTerm = input__element.value.toLowerCase()
-
-  console.log(optionSelected)
-  console.log(isChecked)
-  console.log(searchTerm)
-})
